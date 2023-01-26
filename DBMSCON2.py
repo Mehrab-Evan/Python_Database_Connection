da1b0e9f5b21fdf6b923933f082178c6f9b935be
MD. Mehrab Evan <73308898+Mehrab-Evan@users.noreply.github.com>
2023-01-26 12:28:57 +0600

    Update DBMSCON2.py

--- DBMSCON2.py
+++ DBMSCON2.py
@@ -5,21 +5,28 @@ try:
     connection = mysql.connector.connect(host='localhost',
                                          database='cars',
                                          user='root',
-                                         password='root')
+                                         password='')
+    print("Connection DONE")
+
     if connection.is_connected():
         fname = input()
         lname = input()
         car_model = input()
         db_Info = connection.get_server_info()
-        Query2 = "INSERT INTO carsale ('clients_first_name', 'clients_last_name', 'car_year') VALUES (,%s, %s, %s)"
+
+        Query2 = "INSERT INTO carsale (clients_first_name, clients_last_name, car_year) VALUES (%s, %s, %s)"
         val = (fname, lname, car_model)
         cursor = connection.cursor()
         cursor.execute(Query2, val)
         print("Connected to MySQL Server version ", db_Info)
 
+
+        print("INFO Inserted")
+
         stringQuery = "select * from carsale"
         cursor.execute(stringQuery)
         record = cursor.fetchall()
+
 
         for i in record:
             print(i[0], " ", i[1], " ", i[2], " ", i[3])
