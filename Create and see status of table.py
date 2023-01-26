683c48d69649620fadd0e02de6ca2cf565548249
MD. Mehrab Evan <73308898+Mehrab-Evan@users.noreply.github.com>
2023-01-26 13:50:13 +0600

    Create Create and see status of table.py

---
+++ Create and see status of table.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+connection = mysql.connector.connect(host='localhost',
+                                         database='cars',
+                                         user='root',
+                                         password='')
+mycursor = connection.cursor()
+
+#Table Create
+mycursor.execute("CREATE TABLE studentscg (name VARCHAR(50), age int UNSIGNED, semester VARCHAR(50), cgpa VARCHAR(50), stid int PRIMARY KEY AUTO_INCREMENT)")
+
+#Seeing Status of the table 15 - 17
+mycursor.execute("DESCRIBE studentscg")
+for x in mycursor :
+    print(x)
