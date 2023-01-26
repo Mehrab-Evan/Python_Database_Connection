c0d19ac23a74f68425957ecc7d1056368685aa8a
MD. Mehrab Evan <73308898+Mehrab-Evan@users.noreply.github.com>
2023-01-26 13:56:48 +0600

    Create Insert into DB.py

---
+++ Insert into DB.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+connection = mysql.connector.connect(host='localhost',
+                                         database='cars',
+                                         user='root',
+                                         password='')
+mycursor = connection.cursor()
+
+#mycursor.execute("CREATE TABLE studentscg (name VARCHAR(50), age int UNSIGNED, semester VARCHAR(50), cgpa VARCHAR(50), stid int PRIMARY KEY AUTO_INCREMENT)")
+
+#mycursor.execute("DESCRIBE studentscg")
+
+# for x in mycursor :
+#     print(x)
+
+print("Enter The info of Students")
+name = input("Name : ")
+age = input("AGE : ")
+semester = input("Semester : ")
+cgpa = input("CGPA : ")
+
+mycursor.execute("INSERT INTO studentscg(name, age, semester, cgpa) VALUES(%s, %s, %s, %s)", (name, age, semester, cgpa))
+connection.commit()
